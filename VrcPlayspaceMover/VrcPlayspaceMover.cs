636eb0d058ff01881becbcce18179f66e10c50cb
avail <avail@pomf.se>
2018-12-18 00:56:55 +0100

    init

---
+++ VrcPlayspaceMover/VrcPlayspaceMover.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using UnityEngine;
+
+namespace VrcPlayspaceMover
+{
+    public class VrcPlayspaceMover
+    {
+        public static void Initialize()
+        {
+            new Thread(() =>
+            {
+                Thread.Sleep(7000);
+
+                GameObject obj = new GameObject("VrcPlayspaceMover");
+                obj.AddComponent<Behaviour>();
+                UnityEngine.Object.DontDestroyOnLoad(obj);
+            });
+        }
+
+        // to find OVRInput class: find `OVRGamepad` string
+        private static class OVRInput
+        {
+            // OVRInput.Get() -- find by searching `.None,` (returns: bool)
+            public static bool GetTriggerButton(bool right = true) => AEKOAMDGMGB.MHBKDOFNCBJ(right ? AEKOAMDGMGB.IPJJLPDNABN.One : AEKOAMDGMGB.IPJJLPDNABN.Three); // if right get A otherwise get X
+
+            // OVRInput.GetLocalControllerPosition -- find `().position` (returns: Vector3)
+            public static Vector3 GetLocalControllerPosition(bool right = true) => AEKOAMDGMGB.CJDJIBGLLOG(right ? AEKOAMDGMGB.GJHJJPDDIGG.RTouch : AEKOAMDGMGB.GJHJJPDDIGG.LTouch);
+        }
+
+        public class Behaviour : MonoBehaviour
+        {
+            void Update()
+            {
+                bool leftTrigger = OVRInput.GetTriggerButton(false);
+                bool rightTrigger = OVRInput.GetTriggerButton();
+
+                if (leftTrigger || rightTrigger)
+                {
+                    List<VRCVrCameraOculus> ctrls = ((VRCVrCameraOculus[])UnityEngine.Object.FindObjectsOfType(typeof(VRCVrCameraOculus))).ToList();
+
+                    ctrls.ForEach(ctrl =>
+                    {
+                        if (leftTrigger)
+                        {
+                            ctrl.cameraLiftTransform.localPosition -= new Vector3(0, 0.005f, 0);
+                        }
+
+                        if (rightTrigger)
+                        {
+                            ctrl.cameraLiftTransform.localPosition += new Vector3(0, 0.005f, 0);
+                        }
+                    });
+                }
+            }
+        }
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,15 @@
+hi this is the repository for vrcplayspacemover
+
+this is an attempt to revive playspacemover for vrchat,
+though it has no 3d "hold button and move controller"
+functionality in it yet (i suck at math ok?)
+
+basically, hold A to go up, hold X to go down.
+
+doesn't require any game files to be modified.
+(it uses my [UnityAssemblyInjector](https://github.com/avail/UnityAssemblyInjector) to load the code in)
+
+will be broken when the game updates, unless 
+someone feels like doing reflection for the needed functions (OVRInput class)
+
+downloads in releases tab, drag and drop to vrchat's folder in `Oculus\Software\Software\vrchat-vrchat`.
