e268d8075c903b53fc842b463ac060c1c727de75
avail <avail@pomf.se>
2018-12-24 05:18:06 +0100

    - update for 2018.4.4p3 - cleanup code - make it work (forgot to start the thread!)

--- VrcPlayspaceMover/VrcPlayspaceMover.cs
+++ VrcPlayspaceMover/VrcPlayspaceMover.cs
@@ -5,55 +5,60 @@ using System.Text;
 using System.Threading;
 using UnityEngine;
 
+using OvrInputOrig = LIADNAKJPHD;
+using OvrInputControls = LIADNAKJPHD.NJNOBABHHJN;
+using OvrInputControllers = LIADNAKJPHD.EHEKCPMBABB;
+
+
 namespace VrcPlayspaceMover
 {
     public class VrcPlayspaceMover
     {
+        private static GameObject m_mainObject;
+
         public static void Initialize()
         {
             new Thread(() =>
             {
                 Thread.Sleep(7000);
 
-                GameObject obj = new GameObject("VrcPlayspaceMover");
-                obj.AddComponent<Behaviour>();
-                UnityEngine.Object.DontDestroyOnLoad(obj);
-            });
+                m_mainObject = new GameObject("VrcPlayspaceMover");
+                m_mainObject.AddComponent<Behaviour>();
+
+                UnityEngine.Object.DontDestroyOnLoad(m_mainObject);
+            }).Start();
         }
 
         // to find OVRInput class: find `OVRGamepad` string
         private static class OVRInput
         {
             // OVRInput.Get() -- find by searching `.None,` (returns: bool)
-            public static bool GetTriggerButton(bool right = true) => AEKOAMDGMGB.MHBKDOFNCBJ(right ? AEKOAMDGMGB.IPJJLPDNABN.One : AEKOAMDGMGB.IPJJLPDNABN.Three); // if right get A otherwise get X
+            public static bool GetTriggerButton(bool right = true) => OvrInputOrig.HFCPFKCLFCK(right ? OvrInputControls.One : OvrInputControls.Three); // if right get A otherwise get X
 
             // OVRInput.GetLocalControllerPosition -- find `().position` (returns: Vector3)
-            public static Vector3 GetLocalControllerPosition(bool right = true) => AEKOAMDGMGB.CJDJIBGLLOG(right ? AEKOAMDGMGB.GJHJJPDDIGG.RTouch : AEKOAMDGMGB.GJHJJPDDIGG.LTouch);
+            public static Vector3 GetLocalControllerPosition(bool right = true) => OvrInputOrig.IOLOHLMNMHA(right ? OvrInputControllers.RTouch : OvrInputControllers.LTouch);
         }
 
-        public class Behaviour : MonoBehaviour
+        internal class Behaviour : MonoBehaviour
         {
-            void Update()
+            private void Update()
             {
                 bool leftTrigger = OVRInput.GetTriggerButton(false);
                 bool rightTrigger = OVRInput.GetTriggerButton();
 
                 if (leftTrigger || rightTrigger)
                 {
-                    List<VRCVrCameraOculus> ctrls = ((VRCVrCameraOculus[])UnityEngine.Object.FindObjectsOfType(typeof(VRCVrCameraOculus))).ToList();
+                    VRCVrCameraOculus[] ctrls = (VRCVrCameraOculus[])UnityEngine.Object.FindObjectsOfType(typeof(VRCVrCameraOculus));
 
-                    ctrls.ForEach(ctrl =>
+                    if (leftTrigger)
                     {
-                        if (leftTrigger)
-                        {
-                            ctrl.cameraLiftTransform.localPosition -= new Vector3(0, 0.005f, 0);
-                        }
+                        ctrls[0].cameraLiftTransform.localPosition -= new Vector3(0, Time.deltaTime * 0.2f, 0);
+                    }
 
-                        if (rightTrigger)
-                        {
-                            ctrl.cameraLiftTransform.localPosition += new Vector3(0, 0.005f, 0);
-                        }
-                    });
+                    if (rightTrigger)
+                    {
+                        ctrls[0].cameraLiftTransform.localPosition += new Vector3(0, Time.deltaTime * 0.2f, 0);
+                    }
                 }
             }
         }
