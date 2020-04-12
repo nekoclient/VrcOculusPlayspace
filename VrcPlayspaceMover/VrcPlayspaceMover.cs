1ab5f18749683d0519afe79b274e8e962c98a439
avail <avail@pomf.se>
2020-04-12 15:27:44 +0200

    oops

--- VrcPlayspaceMover/VrcPlayspaceMover.cs
+++ VrcPlayspaceMover/VrcPlayspaceMover.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using UnityEngine;
-
+using System.Diagnostics;
 
 namespace VrcPlayspaceMover
 {
@@ -92,7 +92,7 @@ namespace VrcPlayspaceMover
                     }
                     else
                     {
-                        Log.Debug("camera not found?");
+                        Trace.WriteLine("camera not found?");
                         return;
                     }
 
